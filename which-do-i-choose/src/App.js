2a13010347b788130bf05a14f4839541c3bc3a97
CodeOfGordon <81336844+CodeOfGordon@users.noreply.github.com>
2022-12-21 20:29:11 -0500

    Adjusting to react, creating basic layout of site's input section

--- which-do-i-choose/src/App.js
+++ which-do-i-choose/src/App.js
@@ -1,11 +1,61 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <header className="App-header">
-      Hello world
-    </header>
+    <div className="wrapper">
+      <Header />
+
+      <div className="main-page">
+        <div className="intro">
+          This is a site that will help you decide which of two things you should choose
+        and how much.
+        </div>
+        <br/><br/>
+
+        <form>
+          <label>
+            What currency do you use?
+            <select>
+              <option value="dollar">$</option>
+              <option value="euro">€</option>
+              <option value="pound">£</option>
+              <option value="yen">¥</option>
+              <option value="rupee">₹</option>
+              <option value="won">₩</option>
+              <option value="ruble">₽</option>
+            </select>
+          </label>
+          <br/>
+          <label>
+            What is your budget amount? <input type="number"></input>
+          </label>
+          <br/><br/>
+
+          <label>
+            What is the first thing you want to choose between?
+          </label>
+          <br/>
+          <label>
+            What is its cost per unit?
+          </label>
+          <br/><br/>
+
+          <label>
+            What is the second thing you want to choose between?
+          </label>
+          <br/>
+          <label>
+            What is its cost per unit?
+          </label>
+          
+        </form>
+
+      </div>
+    </div>
   );
 }
+
 
 export default App;
---
+++ which-do-i-choose/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './nav.css';
+
+class Header extends Component {
+    render() {
+        return (
+        <header className='head'>
+            Which Do I Choose?
+        </header>
+        );
+    }
+}
+
+export default Header;
