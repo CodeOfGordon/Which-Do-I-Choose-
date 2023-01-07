08b56c5ec39820de62d1b90439035efbf007d46e
CodeOfGordon <81336844+CodeOfGordon@users.noreply.github.com>
2023-01-06 19:00:07 -0500

    Got the currency and input boxes ready

--- which-do-i-choose/src/App.js
+++ which-do-i-choose/src/App.js
@@ -1,60 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-
-      <div className="main-page">
-        <div className="intro">
-          This is a site that will help you decide which of two things you should choose
-        and how much.
+class App extends Component {
+  render() {
+    function changeCurrency() {
+      var money = document.getElementById("currency-input").value;
+      
+      for (let i = 0; i < document.getElementsByClassName("currency").length; i++) {
+        document.getElementsByClassName("currency")[i].innerHTML = money;  
+        if (money === "₩"){
+          document.getElementsByClassName("currency")[i].style.width = "1.4em";
+        }
+      }
+    }
+  
+    return(
+      <div className="wrapper">
+        
+        <Header />
+  
+        <div className="main-page">
+          <div className="intro">
+            This is a site that will help you decide which of two things you should choose
+          and how much.
+          </div>
+          <br/><br/>
+  
+          <form>
+            <label>
+              What currency do you use?
+              <select id="currency-input" onChange={changeCurrency} defaultValue={"blank"}>
+                <option disabled value="blank" alt="blank"> </option>
+                <option value="$" alt="dollar">$</option>
+                <option value="€" alt="euro">€</option>
+                <option value="£" alt="pound">£</option>
+                <option value="¥" alt="yen">¥</option>
+                <option value="₹" alt="rupee">₹</option>
+                <option value="₩" alt="won">₩</option>
+                <option value="₽" alt="ruble">₽</option>
+              </select>
+            </label>
+            <br/>
+            <label>
+              What is your budget amount?
+              <label className='currency'/><input className='currencyinput' type='number' placeholder='Enter an amount'/>
+            </label>
+            <br/><br/>
+  
+            <label>
+              What is the first thing you want to choose between?
+              <input id='item1' placeholder='Enter an item'/>
+            </label>
+            <br/>
+            <label>
+              What is its cost per unit?
+              <label className='currency'/><input className='currencyinput' type='number' placeholder='Enter the cost'/>
+            </label>
+            <br/><br/>
+  
+            <label>
+              What is the second thing you want to choose between?
+              <input id='item2' placeholder='Enter an item'/>
+            </label>
+            <br/>
+            <label>
+              What is its cost per unit?
+              <label className='currency'/><input className='currencyinput' type='number' placeholder='Enter the cost'/>
+            </label>
+            
+          </form>
+  
         </div>
-        <br/><br/>
-
-        <form>
-          <label>
-            What currency do you use?
-            <select>
-              <option value="dollar">$</option>
-              <option value="euro">€</option>
-              <option value="pound">£</option>
-              <option value="yen">¥</option>
-              <option value="rupee">₹</option>
-              <option value="won">₩</option>
-              <option value="ruble">₽</option>
-            </select>
-          </label>
-          <br/>
-          <label>
-            What is your budget amount? <input type="number"></input>
-          </label>
-          <br/><br/>
-
-          <label>
-            What is the first thing you want to choose between?
-          </label>
-          <br/>
-          <label>
-            What is its cost per unit?
-          </label>
-          <br/><br/>
-
-          <label>
-            What is the second thing you want to choose between?
-          </label>
-          <br/>
-          <label>
-            What is its cost per unit?
-          </label>
-          
-        </form>
-
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 
